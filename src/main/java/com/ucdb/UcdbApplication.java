737de7c950d1cd8c9d4e545a3093bd396344b628
Yuri <yuri.santos@ccc.ufcg.edu.br>
2019-06-12 17:26:17 -0300

    encontrado bugs no caso 1

--- src/main/java/com/ucdb/UcdbApplication.java
+++ src/main/java/com/ucdb/UcdbApplication.java
@@ -2,10 +2,23 @@ package com.ucdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.ucdb.filter.TokenFilter;
 
 @SpringBootApplication
 public class UcdbApplication {
 
+	@Bean
+	public FilterRegistrationBean filterJwt() {
+		FilterRegistrationBean filterRb = new FilterRegistrationBean();
+		filterRb.setFilter(new TokenFilter());
+		filterRb.addUrlPatterns("/private");
+		return filterRb;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UcdbApplication.class, args);
 	}
